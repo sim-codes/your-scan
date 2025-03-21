4c456b3d9b67f3f5f1269489c355b24d4b548c17
Michael <segunmichael24@gmail.com>
2025-03-21 14:49:29 +0100

    Add drawer content with file export options and close functionality in TextEditorScreen

--- screens/main/editor.tsx
+++ screens/main/editor.tsx
@@ -4,6 +4,8 @@ import {
   KeyboardAvoidingView,
   Platform,
   StyleSheet,
+  TouchableOpacity,
+  Text,
 } from 'react-native';
 import { RichText, Toolbar, useEditorBridge, useEditorContent } from '@10play/tentap-editor';
 import { SafeAreaView } from '@/components/common/view';
@@ -12,6 +14,11 @@ import { EditorHeader } from '@/components/common/header';
 import { Drawer } from 'react-native-drawer-layout';
 import { BodyText } from '@/components/common/text';
 import { useNavigation } from '@react-navigation/native';
+import { CustomButton } from '@/components/common/button';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import Feather from '@expo/vector-icons/Feather';
+import Foundation from '@expo/vector-icons/Foundation';
 
 export const TextEditorScreen = () => {
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
@@ -25,6 +32,46 @@ export const TextEditorScreen = () => {
 
   const content = useEditorContent(editor);
 
+  const DrawerContent = () => {
+    return (
+      <View style={tw`flex-1 p-5`}>
+        <TouchableOpacity style={tw`flex-row gap-x-2 items-center border-b border-[#CCE0FF] pb-2 mt-2`} onPress={() => setIsDrawerOpen(false)}>
+          <Feather name="edit" size={24} color="#0066FF" />
+          <BodyText style={tw`text-lg`}>Rename</BodyText>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={tw`flex-row gap-x-2 items-center border-b border-[#CCE0FF] pb-2 mt-2`} onPress={() => setIsDrawerOpen(false)}>
+          <Ionicons name="save-outline" size={24} color="#0066FF" />
+          <BodyText style={tw`text-lg`}>Save</BodyText>
+        </TouchableOpacity>
+
+        <Text style={tw`text-2xl my-2 font-semibold`}>File export options</Text>
+
+        <TouchableOpacity style={tw`flex-row gap-x-2 items-center border-b border-[#CCE0FF] pb-2 mt-2`} onPress={() => setIsDrawerOpen(false)}>
+        <Feather name="file-text" size={24} color="#0066FF" />
+          <BodyText style={tw`text-lg`}>Export to txt</BodyText>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={tw`flex-row gap-x-2 items-center border-b border-[#CCE0FF] pb-2 mt-2`} onPress={() => setIsDrawerOpen(false)}>
+        <Foundation name="page-doc" size={28} color="#0066FF" />
+          <BodyText style={tw`text-lg`}>Export to docx</BodyText>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={tw`flex-row gap-x-2 items-center border-b border-[#CCE0FF] pb-2 mt-2`} onPress={() => setIsDrawerOpen(false)}>
+          <FontAwesome name="file-pdf-o" size={24} color="#0066FF" />
+          <BodyText style={tw`text-lg`}>Export to pdf</BodyText>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          onPressIn={() => navigation.goBack()}
+          style={tw`flex-row gap-x-2 items-center border-b border-[#CCE0FF] pb-2 mt-2`} onPress={() => setIsDrawerOpen(false)}>
+          <Ionicons name="exit-outline" size={24} color="black" />
+          <BodyText style={tw`text-lg text-black`}>Close File</BodyText>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
   return (
     <SafeAreaView padding='none' variant='screen'>
       <Drawer
@@ -32,7 +79,7 @@ export const TextEditorScreen = () => {
         onOpen={() => setIsDrawerOpen(true)}
         onClose={() => setIsDrawerOpen(false)}
         renderDrawerContent={() => {
-            return <BodyText>Drawer content</BodyText>;
+            return <DrawerContent />;
         }}
       >
         <EditorHeader isDrawerOpen={isDrawerOpen} goBack={() => navigation.goBack()} setIsDrawerOpen={setIsDrawerOpen} />
